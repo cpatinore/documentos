2eb625984c2095c45b01e2880a2ed33c6a54f9f8
cpatinore <51925659+cpatinore@users.noreply.github.com>
2021-04-25 11:03:48 -0500

    Add files via upload

--- Ensayo/scritps/urls.py
+++ Ensayo/scritps/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from core import views
+from scritps import views
 
 urlpatterns = [
     path('', views.home, name="home"),
--- Ensayo/scritps/view.py
+++ Ensayo/scritps/view.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Mi Web Personal</h1><h2>Portada</h2>")
+    getoutput("cd raiz")
+    ubicacion= getoutput("pwd")
+
+    return render(request, "index.html",{"ubicacion": ubicacion})
+
+def crear(request):
+    return render(request,"Crear.html")
+
+def borrar(request):
+    return render(request,"Borrar.html")
+
